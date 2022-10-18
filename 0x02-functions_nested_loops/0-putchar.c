46f46a7fe100505e47a6f70f8e29c29e722893cf
Nathan-88 <onwukaebuka@gmail.com>
2022-10-18 09:42:30 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * main - Prints _putchar as a message.
+ * main - print _putchar
  *
- * Return: Always 0 (Success)
+ * this program is written by nathan-88
+ *
+ * Return: 0
  */
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+	int i;
+	char p[] = "_putchar";
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	for (i = 0; i < 8; i++)
 	{
-		putchar(str[count]);
+		_putchar(p[i]);
 	}
 	_putchar('\n');
 	return (0);
