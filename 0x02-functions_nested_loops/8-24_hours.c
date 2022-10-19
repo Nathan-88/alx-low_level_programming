43e13ec516cc02002637197284a94040865dac40
Nathan-88 <onwukaebuka@gmail.com>
2022-10-18 18:53:55 -0700

    error fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,26 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int a, b, c, d;
+	int H, M;
 
-	for (a = 48; a <= 50; a++)
+	for (H = 0; H < 24; H++)
 	{
-		for (b = 48; b <= 57; b++)
+		for (M = 0; M < 60; H++)
 		{
-			for (c = 48; c <= 53; c++)
-			{
-				for (d = 48; d <= 57; d++)
-				{
-					if (a >= 50 && b >= 52)
-						break;
-						_putchar(a);
-						_putchar(b);
-						_putchar(58);
-						_putchar(c);
-						_putchar(d);
-						_putchar('\n');
-				}
-			}
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
