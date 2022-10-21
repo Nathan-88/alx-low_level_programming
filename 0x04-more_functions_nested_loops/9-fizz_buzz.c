b8a060a6cdfb07d07d4adc2721b2cb87fce0ccf1
Nathan-88 <onwukaebuka@gmail.com>
2022-10-21 06:27:56 -0700

    fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,6 @@ int main(void)
 	int i;
 
 	for (i = 1; i < 100; i++)
-	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
@@ -24,13 +23,8 @@ int main(void)
 		}
 		else
 		{
-			printf("%i", i);
+			printf("%d", i);
 		}
-		if (i != 100)
-		{
-			putchar(' ');
-		}
-	}
 	putchar('\n');
 	return (0);
 }
