f9049789ea1b7409dd56907f4083bc98c1e8fd0e
Nathan-88 <onwukaebuka88@gmail.com>
2022-11-27 22:29:02 -0800

    allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	//unsigned int i;
+	unsigned int i;
 	char *arr;
 
 	if (nmemb == 0 || size == 0)
@@ -18,5 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (arr);
+	for (i = 0; i < (nmemb * size); i++)
+		arr[i] = 0;
 	return (arr);
 }
