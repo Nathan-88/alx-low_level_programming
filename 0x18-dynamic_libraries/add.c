b8c7860e3524e8ef1890f211056e443517e00ca7
Nathan-88 <onwukaebuka88@gmail.com>
2023-01-19 08:34:08 -0800

    add function

---
+++ 0x18-dynamic_libraries/add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * add - a function that adds too numbers
+ * @a: first argument passed
+ * @b: second argument passed
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
